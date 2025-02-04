01b2e26a623b9bb061cc33a1568bc7aaf1c5cfa3
CarlosPantin <carlospantinpang@outlook.com>
2025-02-04 18:34:13 +0200

    Analytics

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import './App.css';
-import Header from './components/Header';
-import About from './components/About';
-import Background from './components/Background';
-import Projects from './components/Projects';
+import "./App.css";
+import Header from "./components/Header";
+import About from "./components/About";
+import Background from "./components/Background";
+import Projects from "./components/Projects";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
       <About />
       <Background />
-      <Projects/>
+      <Projects />
+      <Analytics />
     </div>
   );
 }
